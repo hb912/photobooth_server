ef3fd035cd5865dc27ca843bcfaf68e455da06a9
chaehong kim <43055448+hb912@users.noreply.github.com>
2024-07-07 22:33:06 +0900

    initial commit

---
+++ src/test/java/chkim/photobooth/PhotoboothApplicationTests.java
@@ -0,0 +1,13 @@
+package chkim.photobooth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoboothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
